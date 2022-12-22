fcc8b4613c8486b27c867570ce96787a1c64325d
Kh Med <iamkh11@outlook.com>
2022-12-22 15:38:17 +0100

    Update MissionRestController.java
    x1.00

--- src/main/java/com/cubeit/navitrackapi/RestControllers/MissionRestController.java
+++ src/main/java/com/cubeit/navitrackapi/RestControllers/MissionRestController.java
@@ -4,6 +4,7 @@ package com.cubeit.navitrackapi.RestControllers;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,6 +31,7 @@ import reactor.core.publisher.Mono;
 @RestController
 @RequestMapping("/api/cubeIT/NaviTrack/rest/missions")
 @Api(tags ="MissionRestController")
+@CrossOrigin("*")
 public class MissionRestController {
     @Autowired
     private MissionRepository missionRepository;
